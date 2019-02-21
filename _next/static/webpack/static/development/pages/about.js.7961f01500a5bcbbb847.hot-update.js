b75ad716754fab169c523edc45f30215e5bbfa3c
J. Graves <jwgravesfl@gmail.com>
2019-02-20 23:55:30 -0700

    Deploy satm

---
+++ _next/static/webpack/static/development/pages/about.js.7961f01500a5bcbbb847.hot-update.js
@@ -0,0 +1,177 @@
+webpackHotUpdate("static\\development\\pages\\about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Layout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Layout */ "./components/Layout.js");
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var next_seo__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! next-seo */ "./node_modules/next-seo/dist/index.js");
+/* harmony import */ var next_seo__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_seo__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var reactstrap__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! reactstrap */ "./node_modules/reactstrap/es/index.js");
+/* harmony import */ var _static_images_bestOneSoFar_jpg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../static/images/bestOneSoFar.jpg */ "./static/images/bestOneSoFar.jpg");
+/* harmony import */ var _static_images_bestOneSoFar_jpg__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_static_images_bestOneSoFar_jpg__WEBPACK_IMPORTED_MODULE_5__);
+var _jsxFileName = "C:\\Users\\HC\\Desktop\\RevelerMusic\\RevelerMediaServices\\Client Work\\Santa and the Mrs of West Texas\\website\\satm\\satm\\pages\\about.js";
+
+
+
+
+
+
+var AboutDiv = styled_components__WEBPACK_IMPORTED_MODULE_2__["default"].div.withConfig({
+  displayName: "about__AboutDiv",
+  componentId: "sc-10k7tv3-0"
+})(["height:85vh;text-align:left;h1{color:red;}#aboutIntroduction{img{width:10vw;float:right;}}h4{text-align:left;padding-top:1vh;}"]);
+
+var About = function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Layout__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(AboutDiv, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_seo__WEBPACK_IMPORTED_MODULE_3___default.a, {
+    config: {
+      title: "Santa & the Mrs of West Texas",
+      description: "Santa and Mrs Claus services for West Texas"
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39
+    },
+    __self: this
+  }, "About"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_4__["Row"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 40
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_4__["Col"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, "Meet Santa & the Mrs"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    id: "aboutIntroduction",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "With more than 40 years in caring for and nuturing the West Texas area, Santa & the Mrs' heart belong to the West Texas people. Living in an undisclosed location in central West Texas, Santa & the Mrs", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: _static_images_bestOneSoFar_jpg__WEBPACK_IMPORTED_MODULE_5___default.a,
+    alt: "Introduction Image of Santa & the Mrs",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 47
+    },
+    __self: this
+  }), " ", "are centrally located, and the two are more than happy to travel. Although Santa & the Mrs do have a $50 appearance fee, all fees or tips earned will be donated to Toys for Tots or other verified charity. Up to 100% discount on the appearance fee for qualifying charities or good causes."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 57
+    },
+    __self: this
+  }, "Why Do This?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  }, "the Mrs is often heard saying, \"Santa, sometimes people forget the true meaning of Christmas.\" With this though in mind, Santa & the Mrs of West Texas intend to make sure that each and every West Texan he or she meets will be reminded why all of us do this. Santa & the Mrs do this because the Christmas Spirit is meant to be shared. Santa & the Mrs do this to meet the amazing people of the West Texas area. Santa & the Mrs do this because, Mrs Santa, \"It bring Joy to the people\" of West Texas. Come be a part of our family!")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_4__["Col"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 69
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 70
+    },
+    __self: this
+  }, "Santa"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 71
+    },
+    __self: this
+  }, "Santa spends most of his time, when not at work with the elves, spending and enjoying time with the Mrs. Little known fact about Santa Claus is he not only enjoys eating cookies, but Santa also loves to bake cookies. If one is really lucky or good, he or she may be treated with one of Santa's amazing Buck Eyes or maybe one of His nummy Haystacks or Special Ks or the perfectly fluffy peanut brittle #yummyFace.", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", {
+    className: "aboutQuotes",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 79
+    },
+    __self: this
+  }, "Santa says, \"Bake Cookies, Enjoy Family and Be Good. Ho, Ho, Ho!\"")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 83
+    },
+    __self: this
+  }, "the Mrs"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 84
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 85
+    },
+    __self: this
+  }, "Bringing It All Together")))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (About);
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/about")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.7961f01500a5bcbbb847.hot-update.js.map
