695c9ecc3dc5bd012ce7525c25a4a3b7731dd70f
J. Graves <jwgravesfl@gmail.com>
2019-03-01 14:47:42 -0700

    Deploy satm

---
+++ out/_next/static/webpack/static/development/pages/contact.js.320568a200d0f9ca39ec.hot-update.js
@@ -0,0 +1,424 @@
+webpackHotUpdate("static\\development\\pages\\contact.js",{
+
+/***/ "./components/LetterToSanta.js":
+/*!*************************************!*\
+  !*** ./components/LetterToSanta.js ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/slicedToArray */ "./node_modules/@babel/runtime-corejs2/helpers/esm/slicedToArray.js");
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/regenerator */ "./node_modules/@babel/runtime-corejs2/regenerator/index.js");
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _babel_runtime_corejs2_core_js_json_stringify__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/core-js/json/stringify */ "./node_modules/@babel/runtime-corejs2/core-js/json/stringify.js");
+/* harmony import */ var _babel_runtime_corejs2_core_js_json_stringify__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_core_js_json_stringify__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_asyncToGenerator__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/asyncToGenerator */ "./node_modules/@babel/runtime-corejs2/helpers/esm/asyncToGenerator.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var _static_images_stamp_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../static/images/stamp.png */ "./static/images/stamp.png");
+/* harmony import */ var _static_images_stamp_png__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_static_images_stamp_png__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var _static_images_BakingSanta_01_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../static/images/BakingSanta-01.png */ "./static/images/BakingSanta-01.png");
+/* harmony import */ var _static_images_BakingSanta_01_png__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(_static_images_BakingSanta_01_png__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var _material_ui_core_InputLabel__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! @material-ui/core/InputLabel */ "./node_modules/@material-ui/core/InputLabel/index.js");
+/* harmony import */ var _material_ui_core_InputLabel__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(_material_ui_core_InputLabel__WEBPACK_IMPORTED_MODULE_8__);
+/* harmony import */ var _material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! @material-ui/core/Input */ "./node_modules/@material-ui/core/Input/index.js");
+/* harmony import */ var _material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9__);
+/* harmony import */ var _material_ui_core_Radio__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! @material-ui/core/Radio */ "./node_modules/@material-ui/core/Radio/index.js");
+/* harmony import */ var _material_ui_core_Radio__WEBPACK_IMPORTED_MODULE_10___default = /*#__PURE__*/__webpack_require__.n(_material_ui_core_Radio__WEBPACK_IMPORTED_MODULE_10__);
+/* harmony import */ var _material_ui_core_Button__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! @material-ui/core/Button */ "./node_modules/@material-ui/core/Button/index.js");
+/* harmony import */ var _material_ui_core_Button__WEBPACK_IMPORTED_MODULE_11___default = /*#__PURE__*/__webpack_require__.n(_material_ui_core_Button__WEBPACK_IMPORTED_MODULE_11__);
+/* harmony import */ var reactstrap__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! reactstrap */ "./node_modules/reactstrap/es/index.js");
+/* harmony import */ var _HowBehavedChecked__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./HowBehavedChecked */ "./components/HowBehavedChecked.js");
+
+
+
+
+var _jsxFileName = "C:\\Users\\HC\\Desktop\\RevelerMusic\\RevelerMediaServices\\Client Work\\Santa and the Mrs of West Texas\\website\\satm\\satm\\components\\LetterToSanta.js";
+
+
+
+
+
+
+
+
+
+
+
+var LetterToSantaSection = styled_components__WEBPACK_IMPORTED_MODULE_5__["default"].section.withConfig({
+  displayName: "LetterToSanta__LetterToSantaSection",
+  componentId: "cqb60o-0"
+})(["#innerBorder{width:100%;height:100%;border:0.2vw solid black;}#satmPostCard{margin:0;position:absolute;left:50%;transform:translate(-50%,0%);width:40vw;height:25vw;border:1vw dashed rgba(192,8,8,1);outline:0.2vw solid black;box-shadow:-0.3vw 0.3vw 1vw rgba(0,0,0,0.8);}h1{font-family:\"Cookie\",cursive;font-size:4.5vw;text-align:center;}h2{font-family:\"Permanent Marker\",cursive;font-size:2.15vw;text-align:left;padding-left:0.2vw;padding-top:0.2vw;padding-bottom:1vw;width:32vw;text-align:center;}h4{margin:0;padding:0;}p{font-family:\"Special Elite\",cursive;font-size:1.5vw;text-align:left;margin-left:0.5vw;font-variant:small-caps;width:100%;font-weight:800;letter-spacing:0.3vw;margin-top:-1vw;}#imgPostcardStamp{width:5vw;position:absolute;top:1vw;right:1vw;}.inputsSantaPostcard{margin:0 0.2vw 0 0.2vw;}#inputNameIs{width:8vw;}#pYearsOld{padding-right:1vw;text-align:right;}#inputAgeIs{width:2vw;}#rowEmail{font-family:\"Special Elite\",cursive;font-size:1.75vw;text-align:left;margin-left:0.5vw;font-variant:small-caps;width:100%;font-weight:800;letter-spacing:0.3vw;margin-top:-1.5vw;}#rowRadioGroupHowBehaved{text-align:right;padding-right:0.5vw;margin-top:0;}#buttonPostcardSubmit{width:8vw;font-size:0.8vw;padding:0;}.radioButtonsBadorGood{margin-right:1vw;}#inputHowBehaved{width:4vw;font-size:1vw;margin:0;padding:0;}#imgVectorSantaBaking{width:5vw;}#rowPleaseBringMe{margin-top:-1vw;}#inputLabelPleaseBringMe{font-weight:800;margin-left:1.5vw;width:100%;}#inputPleaseBringMe{margin-top:-1vw;width:100%;}#santsGonnaBePleased{font-size:1vw;}#santsGonnaLikeThis{font-size:1vw;}"]);
+
+var onSubmit =
+/*#__PURE__*/
+function () {
+  var _ref = Object(_babel_runtime_corejs2_helpers_esm_asyncToGenerator__WEBPACK_IMPORTED_MODULE_3__["default"])(
+  /*#__PURE__*/
+  _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.mark(function _callee(values) {
+    return _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_1___default.a.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return sleep(300);
+
+          case 2:
+            window.alert(_babel_runtime_corejs2_core_js_json_stringify__WEBPACK_IMPORTED_MODULE_2___default()(values, 0, 2));
+
+          case 3:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function onSubmit(_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+function LetterToSanta() {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_4__["useState"])("nice"),
+      _useState2 = Object(_babel_runtime_corejs2_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_0__["default"])(_useState, 2),
+      howBehaved = _useState2[0],
+      setHowBehaved = _useState2[1];
+
+  var isEnabled = howBehaved === "naughty";
+  return react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(LetterToSantaSection, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 165
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 166
+    },
+    __self: this
+  }, "Contact Santa & the Mrs of West Texas"), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("section", {
+    id: "satmPostCard",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 168
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("section", {
+    id: "innerBorder",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 169
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("h2", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 170
+    },
+    __self: this
+  }, "Postcard to Santa & the Mrs"), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("img", {
+    id: "imgPostcardStamp",
+    src: _static_images_stamp_png__WEBPACK_IMPORTED_MODULE_6___default.a,
+    alt: "",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 172
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 174
+    },
+    __self: this
+  }, "My name is", react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default.a, {
+    id: "inputNameIs",
+    className: "inputsSantaPostcard",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 176
+    },
+    __self: this
+  }), ","), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("p", {
+    id: "pYearsOld",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 179
+    },
+    __self: this
+  }, "and I am", react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default.a, {
+    id: "inputAgeIs",
+    className: "inputsSantaPostcard",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 181
+    },
+    __self: this
+  }), "years old."), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 185
+    },
+    __self: this
+  }, "I have been", react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default.a, {
+    disabled: true,
+    defaultValue: "Choose 1",
+    id: "inputHowBehaved",
+    className: "inputsSantaPostcard",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 187
+    },
+    __self: this
+  }), "this year."), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Row"], {
+    id: "rowEmail",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 195
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 4,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 196
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_InputLabel__WEBPACK_IMPORTED_MODULE_8___default.a, {
+    id: "inputLabelEmail",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 197
+    },
+    __self: this
+  }, "Email Address:")), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 8,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 199
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default.a, {
+    style: {
+      width: "100%"
+    },
+    id: "inputEmail",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 200
+    },
+    __self: this
+  }))), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Row"], {
+    id: "rowRadioGroupHowBehaved",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 203
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 4,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 204
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Button__WEBPACK_IMPORTED_MODULE_11___default.a, {
+    variant: "contained",
+    disabled: isEnabled,
+    id: "buttonPostcardSubmit",
+    type: "submit",
+    value: "Submit",
+    color: "secondary",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 205
+    },
+    __self: this
+  }, "Send To Santa")), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 8,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 216
+    },
+    __self: this
+  }, "Bad", react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Radio__WEBPACK_IMPORTED_MODULE_10___default.a, {
+    value: "naughty",
+    onChange: function onChange(event) {
+      return setHowBehaved(event.target.value);
+    },
+    checked: howBehaved === "naughty",
+    className: "radioButtonsBadorGood",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 218
+    },
+    __self: this
+  }), "Good", react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Radio__WEBPACK_IMPORTED_MODULE_10___default.a, {
+    value: "nice",
+    onChange: function onChange(event) {
+      return setHowBehaved(event.target.value);
+    },
+    checked: howBehaved === "nice",
+    className: "radioButtonsBadorGood",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 225
+    },
+    __self: this
+  }), "Very Good", react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Radio__WEBPACK_IMPORTED_MODULE_10___default.a, {
+    value: "veryNice",
+    onChange: function onChange(event) {
+      return setHowBehaved(event.target.value);
+    },
+    checked: howBehaved === "veryNice",
+    className: "radioButtonsBadorGood",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 232
+    },
+    __self: this
+  }))), howBehaved === "naughty" && react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_HowBehavedChecked__WEBPACK_IMPORTED_MODULE_13__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 240
+    },
+    __self: this
+  }), howBehaved === "nice" && react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Row"], {
+    id: "rowNice",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 242
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 3,
+    id: "santsGonnaLikeThis",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 243
+    },
+    __self: this
+  }, "Santa's gonna like this!"), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 6,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 246
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("h4", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 247
+    },
+    __self: this
+  }, "You've Been Good!"), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 248
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default.a, {
+    style: {
+      padding: "0",
+      margin: "0"
+    },
+    className: "inputPleaseBringMe",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 249
+    },
+    __self: this
+  }))), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 3,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 255
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("img", {
+    id: "imgVectorSantaBaking",
+    src: _static_images_BakingSanta_01_png__WEBPACK_IMPORTED_MODULE_7___default.a,
+    alt: "",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 256
+    },
+    __self: this
+  }))), howBehaved === "veryNice" && react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Row"], {
+    id: "rowNice",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 265
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 3,
+    id: "santsGonnaBePleased",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 266
+    },
+    __self: this
+  }, "Santa's Gonna Be Pleased!"), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 6,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 269
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("h4", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 270
+    },
+    __self: this
+  }, "You've Been Good!"), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 271
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default.a, {
+    className: "inputPleaseBringMe",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 272
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(_material_ui_core_Input__WEBPACK_IMPORTED_MODULE_9___default.a, {
+    className: "inputPleaseBringMe",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 273
+    },
+    __self: this
+  }))), react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(reactstrap__WEBPACK_IMPORTED_MODULE_12__["Col"], {
+    xs: 3,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 276
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("img", {
+    id: "imgVectorSantaBaking",
+    src: _static_images_BakingSanta_01_png__WEBPACK_IMPORTED_MODULE_7___default.a,
+    alt: "",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 277
+    },
+    __self: this
+  }))))));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (LetterToSanta);
+
+/***/ })
+
+})
+//# sourceMappingURL=contact.js.320568a200d0f9ca39ec.hot-update.js.map
